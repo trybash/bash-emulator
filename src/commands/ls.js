be4265da5edcbc87413f288a5c3f0ea700fcc739
Jorin Vogel <hi@jorin.me>
2016-06-27 16:51:23 +0200

    ls with args. inlcuding tests.

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,21 +1,32 @@
 require('string.prototype.startswith')
 require('string.prototype.includes')
 
+// NOTE: No support for files yet
 function ls (env, args) {
-  var state = env.system.state
-  var cwd = state.workingDirectory
-  var listing = Object.keys(state.fileSystem)
-    .filter(function (path) {
-      return path.startsWith(cwd) && path !== cwd
+  var task
+  if (!args.length) {
+    task = env.system.getDir()
+      .then(env.system.readDir)
+      .then(function (listing) {
+        return listing.join(' ')
+      })
+  } else if (args.length === 1) {
+    task = env.system.readDir(args[0]).then(function (listing) {
+      return listing.join(' ')
     })
-    .map(function (path) {
-      return path.substr(cwd === '/' ? cwd.length : cwd.length + 1)
+  } else {
+    task = Promise.all(args.sort().map(function (path) {
+      return env.system.readDir(path).then(function (listing) {
+        return path + ':\n' + listing.join(' ')
+      })
+    })).then(function (listings) {
+      return listings.join('\n\n')
     })
-    .filter(function (path) {
-      return !path.includes('/')
-    })
-  env.output(listing.join(' '))
-  env.exit()
+  }
+  task.then(function (result) {
+    env.output(result)
+    env.exit()
+  })
 }
 
 module.exports = ls
---
+++ test/commands/ls.js
@@ -0,0 +1,54 @@
+var test = require('tape')
+var bashEmulator = require('../../src')
+
+test('ls', function (t) {
+  t.plan(3)
+  var testState = {
+    history: [],
+    user: 'test',
+    workingDirectory: '/',
+    fileSystem: {
+      '/': {
+        type: 'dir',
+        lastEdited: Date.now()
+      },
+      '/etc': {
+        type: 'dir',
+        lastEdited: Date.now()
+      },
+      '/home': {
+        type: 'dir',
+        lastEdited: Date.now()
+      },
+      '/home/test': {
+        type: 'dir',
+        lastEdited: Date.now()
+      },
+      '/home/test/README': {
+        type: 'file',
+        lastEdited: Date.now(),
+        content: 'read this first'
+      }
+    }
+  }
+  var emulator = bashEmulator(testState)
+  emulator.run('ls').then(function (output) {
+    t.equal(output, 'etc home', 'without args')
+  })
+  emulator.run('ls home').then(function (output) {
+    t.equal(output, 'test', 'list dir')
+  })
+  emulator.run('ls home /').then(function (output) {
+    var listing =
+      '/:' +
+      '\n' +
+      'etc home' +
+      '\n' +
+      '\n' +
+      'home:' +
+      '\n' +
+      'test'
+    t.equal(output, listing, 'list multiple')
+  })
+})
+
